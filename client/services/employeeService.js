aab015af026a6f50a0f4e38d8bf286f0add2e95e
User <User@WRSD-LD309-026.dut.ac.za>
2024-11-20 14:42:33 +0200

    feat vite added, app.css updated

---
+++ client/services/employeeService.js
@@ -0,0 +1,15 @@
+ export const addEmployee = async (emoloyee,photoFile) => {
+    const formData = new FormData();
+    formData.append("name", emoloyee.name);
+    formData.append("surname", emoloyee.surname);
+    formData.append("email", emoloyee.email);
+    formData.append("idNumber", emoloyee.idNumber);
+    formData.append("position", emoloyee.position);
+
+    const response = await fetch("http://localhost:3000/api/employees", {
+        method: "POST",
+        body: formData,
+    });
+
+    return response.json();
+ }
---
+++ client/src/components/add.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { addEmployee } from "../services/employeeService";
+
+function Add() {
+    const  [employeeData, setEmployeeData] = useState ({
+        name: "",
+        surname: "",
+        email: "",
+        idNumber: "",
+        position: ""
+    })
+
+    const [photo, setPhoto] = useState(null)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const newEmployee = await addEmployee(employeeData, photo)
+        console.log("Employee added", newEmployee)
+
+        return(
+            <div>
+                <h1>Employee Information</h1>
+                <form onSubmit={handleSubmit}>
+                    <lable>Name</lable>
+                    <input type="text" placeholder="First Name" value={employeeData.name} onChange={(e) => setEmployeeData({...employeeData, name: e.target.value})} required/>
+                    <input type="text" placeholder="Last Name" value={employeeData.surname} onChange={(e) => setEmployeeData({...employeeData, surname: e.target.value})} required/>
+                    <input type="text" placeholder="Email Address" value={employeeData.email} onChange={(e) => setEmployeeData({...employeeData, email: e.target.value})} required/>
+                    <input type="text" placeholder="ID Number" value={employeeData.idNumber} onChange={(e) => setEmployeeData({...employeeData, idNumber: e.target.value})} required/>
+                    <input type="text" placeholder="Position" value={employeeData.position} onChange={(e) => setEmployeeData({...employeeData, position: e.target.value})} required/>
+                    <input type="file" onChange={(e) => setPhoto(e.target.files[0])}/>
+                    <button type="submit">Add Employee</button>
+                </form>
+            </div>
+        )
+    }
+}
