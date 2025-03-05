a766cb1e6cc59450f0739eeca3be4dc629e58e63
User <User@WRSD-LD309-026.dut.ac.za>
2025-03-05 10:40:16 +0200

    feat services moved to src

--- client/src/components/add.jsx
+++ client/src/components/add.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { addEmployee } from "";
+import { addEmployee } from "../services/employeeService.js";
 
 function Add() {
     const  [employeeData, setEmployeeData] = useState ({
